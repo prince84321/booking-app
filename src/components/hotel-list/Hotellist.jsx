6b579cb7ba6e02223d6220e9c7994aae539855d7
prince84321 <friendlyprince@gmail.com>
2023-01-19 18:11:39 +0530

    booking

---
+++ src/components/hotel-list/Hotellist.jsx
@@ -0,0 +1,310 @@
+import { Link, NavLink } from "react-router-dom";
+import "./hotellist.css";
+
+function Hotellist() {
+  return (
+    <div className="hotel-list-container">
+
+  
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+            <NavLink to="/hotel">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/287476409.webp?k=dced7cbea2d3e9d0c6ddd32bd5578cbb7499e778cbf069f416736e940be85b89&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+                 </NavLink>
+            </div>
+            <div className="hotel-list-center">
+            <NavLink to="/hotel" className="nav-heading">
+                <h3 className="hotel-list-center-title">
+                Hotel DVIJ INN
+                </h3>
+                </NavLink>
+                <span>2 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Delux Room</h5>
+                <span>1 queen bed</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Excellent</h4>
+                    <span>80 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">8.8</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 10,100</h4>
+                    <span>+₹1,212 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/228083865.webp?k=2fe482dba71d5a40fbf526956a3c675beda196e75263e9f59236448883fbcfe9&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                
+                  Kapish Home-All Pure Veg
+
+                </h3>
+                <span>500 meter from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Delux Double Room</h5>
+                <span>1 full bed</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Good</h4>
+                    <span>43 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">7.2</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 12,221</h4>
+                    <span>+₹ 1,212 taxes and charges</span>
+                    
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                   
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/94387491.webp?k=23f2cab465f20212f03403e6d38fa0cb9aa1a4194e5b597407c031af3ca8cf8a&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                Rawla Rawatsar
+                </h3>
+                <span>2.3 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Family Room</h5>
+                <span>2 beds (1 twin, 1 king)</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Wonderful</h4>
+                    <span>29 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">9.0</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 4,670</h4>
+                    <span> +₹ 1,212 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/282913119.webp?k=6c0e168f13531466901964d1bf536c7031f945da04b5dd7f14e67b6bf304b2ac&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                Hotel Myriad Inn
+                </h3>
+                <span>1.3 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Delux double Room</h5>
+                <span>1 queen bed</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Wonderful</h4>
+                    <span>126 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">9.2</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 7,800</h4>
+                    <span>+₹ 9,36 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/378399608.webp?k=4fd4fd396e7b0cfb6e1f14b26581c852e9e06bdc9ef349f40a00bd1cd96e089f&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                AnandMai - Sustainable Living Spaces
+                </h3>
+                <span>2 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Family Room</h5>
+                <span>3 bunk beds</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Good</h4>
+                    <span>67 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">6.8</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 9,145</h4>
+                    <span>+₹ 1,212 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/415580431.webp?k=2ca50e370321015ac858ea53370d5bb55539c3a46588c1288685b159d42241ea&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                The Experia - A Boutique Hotel
+                </h3>
+                <span>7.7 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Deluxe Double Room</h5>
+                <span>1 full bed</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Excellent</h4>
+                    <span>80 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">8.8</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 13,140</h4>
+                    <span>₹ 1,212 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="hotel-list">
+        <div className="hetel-list-wrapper">
+            <div className="hotel-list-left">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/287476409.webp?k=dced7cbea2d3e9d0c6ddd32bd5578cbb7499e778cbf069f416736e940be85b89&o=&s=1"
+                className="hotel-list-img"
+                alt="" />
+            </div>
+            <div className="hotel-list-center">
+                <h3 className="hotel-list-center-title">
+                Hotel DVIJ INN
+                </h3>
+                <span>2 km from center</span>
+                <span className="hotel-list-taxi">Free Airport taxi</span>
+                <h5>Delux Room</h5>
+                <span>1 queen bed</span>
+                <h4>FREE cancellation • No prepayment needed</h4>
+                <span className="hotel-list-cancel">You can cancel later, so lock in this great price today!</span>
+
+            </div>
+            <div className="hotel-list-right">
+                <div className="hotel-list-right-rating">
+                    <div className="hotel-list-right-rating-text">
+
+                    <h4>Excellent</h4>
+                    <span>80 reviews</span>
+                    </div>
+                    <button className="hotel-list-right-btn">8.8</button>
+                </div>
+                <div className="hotel-list-right-bottom">
+                    <span>3 nights, 2 adults</span>
+                    <h4>₹ 10,100</h4>
+                    <span>₹ 1,212 taxes and charges</span>
+                    <button className="hotel-list-right-bottom-btn">
+                    <NavLink to="/hotel" className="nav-heading">
+                        See avaibility
+                    </NavLink>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+                 
+
+export default Hotellist;
